08c7122465932abb80bab6177ad1dec1ae7ad309
sam <samuelironkwec@gmail.com>
2024-01-27 19:41:05 -0600

    x

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -151,19 +151,16 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing }) => {
       className={`
     ${
       selectedTrip.id === trip.id
-        ? ' border rounded-lg border-blue-500'
+        ? ' '
         : `border-b border-zinc-700 ${i % 2 && 'bg-zinc-800'}`
     }`}
     >
-      <td className="flex items-center">
-        <input
-          checked
-          id="default-radio-2"
-          type="radio"
-          value=""
-          name="default-radio"
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-        />
+      <td className="flex pl-2 items-center">
+        {selectedTrip.id === trip.id ? (
+          <input  type='radio' className="w-4 h-4 border border-blue-500 bg-blue-500 p-1 " />
+        ) : (
+          <div className="w-4 h-4 bg-gray-100 border-gray-300" />
+        )}
       </td>
       <th scope="row" className="pl-10 py-4 font-medium whitespace-nowrap">
         {trip.date}
