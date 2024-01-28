6e367252ba83d20cef7cbfa759e3894ce0c4dd33
sam <samuelironkwec@gmail.com>
2024-01-27 20:33:50 -0600

    x

--- components/Table.tsx
+++ components/Table.tsx
@@ -30,9 +30,9 @@ const Table = ({
     >
       <td className=" pl-2 py-4 ">
         {selectedTrip.id === trip.id ? (
-          <span className="w-4 h-4 rounded-full border border-blue-500 bg-blue-500 p-2 " />
+          <input className="w-4 h-4 rounded-full border border-blue-500 bg-blue-500 p-2 " />
         ) : (
-          <span className="w-4 h-4 rounded-full bg-gray-100 border-gray-300" />
+          <input className="w-4 h-4 rounded-full bg-gray-100 border-gray-300" />
         )}
       </td>
       <th scope="row" className="pl-10 py-4 font-medium whitespace-nowrap">
