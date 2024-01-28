9c6cb20eea32a0d762035c172dc6219b16a1ab38
sam <samuelironkwec@gmail.com>
2024-01-27 19:24:55 -0600

    x

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -145,6 +145,9 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing }) => {
 
   const renderTripRows = listing.activeTrips?.map((trip, i) => (
     <tr
+      onClick={() => {
+        setSelectedTrip(trip);
+      }}
       className={`
     ${
       selectedTrip.id === trip.id
@@ -152,29 +155,33 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing }) => {
         : `border-b border-zinc-700 ${i % 2 && 'bg-zinc-800'}`
     }`}
     >
-      <span
-        onClick={() => {
-          setSelectedTrip(trip);
-        }}
-      >
-        <th scope="row" className="pl-10 py-4 font-medium whitespace-nowrap">
-          {trip.date}
-        </th>
-        <td className="pl-10 py-4">
-          {loadedPrices?.get(trip.id) ? (
-            loadedPrices.get(trip.id)
-          ) : (
-            <button
-              onClick={() => getPrice(trip)}
-              disabled={priceIsLoading}
-              className="text-sm px-2 py-1 bg-zinc-100 text-black rounded-lg shadow active:bg-zinc-300 transition duration-150 transform active:scale-110"
-            >
-              show
-            </button>
-          )}
-        </td>
-        <td className="pl-10 py-4 text-zinc-400">{trip.user_ids?.length}</td>
-      </span>
+      <td className="flex items-center">
+        <input
+          checked
+          id="default-radio-2"
+          type="radio"
+          value=""
+          name="default-radio"
+          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+        />
+      </td>
+      <th scope="row" className="pl-10 py-4 font-medium whitespace-nowrap">
+        {trip.date}
+      </th>
+      <td className="pl-10 py-4">
+        {loadedPrices?.get(trip.id) ? (
+          loadedPrices.get(trip.id)
+        ) : (
+          <button
+            onClick={() => getPrice(trip)}
+            disabled={priceIsLoading}
+            className="text-sm px-2 py-1 bg-zinc-100 text-black rounded-lg shadow active:bg-zinc-300 transition duration-150 transform active:scale-110"
+          >
+            show
+          </button>
+        )}
+      </td>
+      <td className="pl-10 py-4 text-zinc-400">{trip.user_ids?.length}</td>
     </tr>
   ));
 
@@ -224,6 +231,7 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing }) => {
                 <table className="w-full text-sm text-left rtl:text-right text-zinc-300">
                   <thead className="text-xs text-white uppercase bg-zinc-900">
                     <tr className="">
+                      <th scope="col" className="pl-2 py-3"></th>{' '}
                       <th scope="col" className="pl-10 py-3">
                         Time
                       </th>
@@ -238,24 +246,6 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing }) => {
                   <tbody>{renderTripRows}</tbody>
                 </table>
               </div>{' '}
-              <div className="relative max-w-2xl mx-auto mt-8 shadow-md overflow-x-auto rounded-lg">
-                <table className="w-full text-sm text-left rtl:text-right text-zinc-300">
-                  <thead className="text-xs text-white uppercase bg-zinc-900">
-                    <tr className="">
-                      <th scope="col" className="pl-10 py-3">
-                        Time
-                      </th>
-                      <th scope="col" className="pl-10 py-3">
-                        Price
-                      </th>
-                      <th scope="col" className="pl-10 py-3">
-                        Riders
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>{renderTripRows}</tbody>
-                </table>
-              </div>
             </div>
             {window.innerWidth > 640 && (
               <div className="h-[450px] min-h-[1em] w-px self-stretch bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-10"></div>
